9c355784ab2649f9f2592450dc18ea604d6ae214
Natalia Orsi <naty.m.orsi@gmail.com>
2024-02-05 16:24:16 +0100

    Removed unused vars

--- photo-share-frontend/src/App.js
+++ photo-share-frontend/src/App.js
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import Login from './components/Login';
 import Home from './container/Home';
-import { fetchUser } from './utils/fetchUser';
 
 const App = () => {
   const navigate = useNavigate();
